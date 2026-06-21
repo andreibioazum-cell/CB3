c107fc2203fb0c5ea616a12573ff7852f4ec392b
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-21 11:55:16 +0400

    Update lobby.cpp

--- lobby.cpp
+++ lobby.cpp
@@ -1,58 +1,67 @@
 #include "lobby.hpp"
+#include <SDL2/SDL_ttf.h>
 
 namespace lobby {
 
-void Lobby::init(sf::RenderWindow& win) {
-    window = &win;
-    font.loadFromFile("assets/Fredoka-Bold.ttf");
-    
-    title.setFont(font);
-    title.setString("CUBIC BATTLE");
-    title.setCharacterSize(64);
-    title.setFillColor(sf::Color::White);
-    title.setPosition(640 - title.getLocalBounds().width/2, 200);
-    
-    subtitle.setFont(font);
-    subtitle.setString("Touch & Dodge");
-    subtitle.setCharacterSize(22);
-    subtitle.setFillColor(sf::Color::White);
-    subtitle.setPosition(640 - subtitle.getLocalBounds().width/2, 280);
-    
-    playBtn.setSize({220, 75});
-    playBtn.setFillColor(sf::Color(140, 51, 217));
-    playBtn.setPosition(640 - 110, 400);
-    playBtn.setOutlineThickness(3);
-    playBtn.setOutlineColor(sf::Color::Black);
-    
-    playText.setFont(font);
-    playText.setString("PLAY");
-    playText.setCharacterSize(30);
-    playText.setFillColor(sf::Color::White);
-    playText.setPosition(640 - playText.getLocalBounds().width/2, 422);
-    
-    gradientBg.setSize({1280, 720});
-    gradientBg.setFillColor(sf::Color(115, 38, 204));
+void Lobby::init(SDL_Renderer* ren) {
+    renderer = ren;
+    TTF_Init();
+    font = TTF_OpenFont("assets/Fredoka-Bold.ttf", 64);
 }
 
 bool Lobby::update(float dt) {
     return startGame;
 }
 
-void Lobby::draw(sf::RenderWindow& w) {
-    w.draw(gradientBg);
-    w.draw(title);
-    w.draw(subtitle);
-    w.draw(playBtn);
-    w.draw(playText);
+void Lobby::draw(SDL_Renderer* ren) {
+    // Градиентный фон
+    SDL_SetRenderDrawColor(ren, 115, 38, 204, 255);
+    SDL_RenderFillRect(ren, nullptr);
+    
+    if (!font) return;
+    
+    // Заголовок
+    SDL_Color white = {255, 255, 255, 255};
+    SDL_Surface* surf = TTF_RenderText_Solid(font, "CUBIC BATTLE", white);
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, surf);
+    
+    SDL_Rect rect = {640 - surf->w/2, 200, surf->w, surf->h};
+    SDL_RenderCopy(ren, tex, nullptr, &rect);
+    
+    SDL_DestroyTexture(tex);
+    SDL_FreeSurface(surf);
+    
+    // Кнопка Play
+    SDL_SetRenderDrawColor(ren, 140, 51, 217, 255);
+    SDL_Rect btn = {530, 400, 220, 75};
+    SDL_RenderFillRect(ren, &btn);
+    
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+    SDL_RenderDrawRect(ren, &btn);
+    
+    TTF_Font* smallFont = TTF_OpenFont("assets/Fredoka-Bold.ttf", 30);
+    surf = TTF_RenderText_Solid(smallFont, "PLAY", white);
+    tex = SDL_CreateTextureFromSurface(ren, surf);
+    
+    rect = {640 - surf->w/2, 420, surf->w, surf->h};
+    SDL_RenderCopy(ren, tex, nullptr, &rect);
+    
+    SDL_DestroyTexture(tex);
+    SDL_FreeSurface(surf);
+    TTF_CloseFont(smallFont);
 }
 
-void Lobby::handleEvent(const sf::Event& e) {
-    if (e.type == sf::Event::TouchBegan || e.type == sf::Event::MouseButtonPressed) {
-        sf::Vector2f pos;
-        if (e.type == sf::Event::TouchBegan) pos = {e.touch.x, e.touch.y};
-        else pos = {e.mouseButton.x, e.mouseButton.y};
-        
-        if (pos.x > 530 && pos.x < 750 && pos.y > 400 && pos.y < 475) {
+void Lobby::handleEvent(const SDL_Event& e) {
+    if (e.type == SDL_MOUSEBUTTONDOWN) {
+        int x = e.button.x, y = e.button.y;
+        if (x > 530 && x < 750 && y > 400 && y < 475) {
+            startGame = true;
+        }
+    }
+    if (e.type == SDL_FINGERDOWN) {
+        float x = e.tfinger.x * 1280;
+        float y = e.tfinger.y * 720;
+        if (x > 530 && x < 750 && y > 400 && y < 475) {
             startGame = true;
         }
     }
