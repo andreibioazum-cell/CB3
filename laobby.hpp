e2a7a78d1f53221706a187265726a7d3232bbe9a
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-21 10:29:14 +0400

    Create laobby.hpp

---
+++ laobby.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Lobby {
+private:
+    sf::RenderWindow* window;
+    sf::Font font;
+    sf::Text title, subtitle, playText;
+    sf::RectangleShape playBtn, gradientBg;
+    bool startGame = false;
+    
+public:
+    void init(sf::RenderWindow& win);
+    bool update(float dt);
+    void draw(sf::RenderWindow& w);
+    void handleEvent(const sf::Event& e);
+};
