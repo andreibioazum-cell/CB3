b4013366057c75d763bcfeb8f9ac6a5e8a9a61af
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-21 12:04:37 +0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,26 +1,53 @@
 #include "game.hpp"
 #include <cmath>
-#include <SDL2/SDL_image.h>
+#include <android/log.h>
+
+#define LOG_TAG "Game"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 namespace game {
 
-void Game::init(SDL_Renderer* ren) {
-    renderer = ren;
+// Шейдеры (такие же как в lobby)
+const char* vertexShader = 
+    "attribute vec3 aPos;"
+    "uniform mat4 uTransform;"
+    "uniform vec4 uColor;"
+    "varying vec4 vColor;"
+    "void main() {"
+    "   gl_Position = uTransform * vec4(aPos, 1.0);"
+    "   vColor = uColor;"
+    "}";
+
+const char* fragmentShader = 
+    "precision highp float;"
+    "varying vec4 vColor;"
+    "void main() {"
+    "   gl_FragColor = vColor;"
+    "}";
+
+void Game::createShaderProgram() {
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShader, nullptr);
+    glCompileShader(vs);
     
-    // Загрузка текстур
-    SDL_Surface* surf = IMG_Load("assets/player.png");
-    if (surf) {
-        playerTex = SDL_CreateTextureFromSurface(ren, surf);
-        SDL_FreeSurface(surf);
-    }
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShader, nullptr);
+    glCompileShader(fs);
     
-    surf = IMG_Load("assets/grass.png");
-    if (surf) {
-        bgTex = SDL_CreateTextureFromSurface(ren, surf);
-        SDL_FreeSurface(surf);
-    }
+    program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
     
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+}
+
+void Game::init() {
+    createShaderProgram();
+    enemy.init();
     reset();
+    LOGI("Game initialized!");
 }
 
 void Game::reset() {
@@ -31,14 +58,13 @@ void Game::reset() {
     dead = false;
     backToLobby = false;
     bullets.clear();
-    camX = 0; camY = 0;
     enemy.reset();
 }
 
 void Game::update(float dt) {
     if (dead) return;
     
-    // Движение
+    // Движение игрока
     if (controls.joyActive) {
         float dx = controls.joyStickX - controls.joyX;
         float dy = controls.joyStickY - controls.joyY;
@@ -52,7 +78,6 @@ void Game::update(float dt) {
         }
     }
     
-    // Атака
     controls.atkPress += (controls.atkHold ? 1 : -1) * dt * 12;
     controls.atkPress = std::max(0.0f, std::min(1.0f, controls.atkPress));
     
@@ -72,7 +97,7 @@ void Game::update(float dt) {
     }
     
     // Враг
-    enemy.update(dt, {playerX, playerY}, bullets, [this](int dmg) {
+    enemy.update(dt, playerX, playerY, bullets, [this](int dmg) {
         hp -= dmg;
         hitTimer = 1;
         if (hp <= 0) {
@@ -85,44 +110,94 @@ void Game::update(float dt) {
     hitTimer = std::max(0.0f, hitTimer - dt * 3);
 }
 
-void Game::draw(SDL_Renderer* ren) {
-    // Мир (камера)
-    // Здесь OpenGL или SDL_RenderCopy с камерой
+void Game::drawRect(float x, float y, float w, float h, float r, float g, float b) {
+    glUseProgram(program);
     
-    // Игрок
-    SDL_Rect rect = {(int)(playerX - camX - 27.5f), 
-                     (int)(playerY - camY - 27.5f), 55, 55};
-    SDL_RenderCopyEx(ren, playerTex, nullptr, &rect, 
-                     playerAngle * 180 / 3.14159f, nullptr, SDL_FLIP_NONE);
+    float ortho[16] = {
+        2.0f/1280, 0, 0, 0,
+        0, -2.0f/720, 0, 0,
+        0, 0, 1, 0,
+        -1, 1, 0, 1
+    };
     
+    GLint transformLoc = glGetUniformLocation(program, "uTransform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, ortho);
+    
+    GLint colorLoc = glGetUniformLocation(program, "uColor");
+    glUniform4f(colorLoc, r, g, b, 1.0f);
+    
+    float vertices[] = {
+        x-w/2, y-h/2,
+        x+w/2, y-h/2,
+        x+w/2, y+h/2,
+        x-w/2, y+h/2
+    };
+    
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    GLint posLoc = glGetAttribLocation(program, "aPos");
+    glEnableVertexAttribArray(posLoc);
+    glVertexAttribPointer(posLoc, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    
+    glDeleteBuffers(1, &vbo);
+}
+
+void Game::draw() {
+    glClearColor(0.1f, 0.05f, 0.2f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Рисуем игрока (синий квадрат)
+    drawRect(playerX - camX, playerY - camY, 55, 55, 0.3f, 0.85f, 0.35f);
+    
+    // Рисуем врага
+    enemy.draw();
+    
+    // HP бар
+    drawHPBar(1060, 30, 200, 18, hp, PLAYER_HP_MAX);
+}
+
+void Game::drawHPBar(float x, float y, float w, float h, int hp, int max) {
+    // Фон
+    drawRect(x + w/2, y + h/2, w, h, 0.0f, 0.0f, 0.0f);
     // HP
-    drawHPBar(ren, 1060, 30, hp, PLAYER_HP_MAX);
+    drawRect(x + w/2 - (w/2)*(1 - (float)hp/max), y + h/2, 
+             w * (float)hp/max, h, 0.3f, 0.85f, 0.35f);
 }
 
-void Game::drawHPBar(SDL_Renderer* ren, int x, int y, int hp, int max) {
-    SDL_SetRenderDrawColor(ren, 0, 0, 0, 128);
-    SDL_Rect bg = {x, y, 200, 18};
-    SDL_RenderFillRect(ren, &bg);
-    
-    SDL_SetRenderDrawColor(ren, 76, 217, 89, 255);
-    SDL_Rect bar = {x, y, (int)(200 * (float)hp/max), 18};
-    SDL_RenderFillRect(ren, &bar);
-}
-
-void Game::spawnBullet(float dx, float dy) {
-    Bullet b;
-    b.x = playerX; b.y = playerY;
-    b.vx = dx * BULLET_SPEED;
-    b.vy = dy * BULLET_SPEED;
-    bullets.push_back(b);
-}
-
-void Game::handleEvent(const SDL_Event& e) {
-    if (e.type == SDL_MOUSEBUTTONDOWN) {
-        int x = e.button.x, y = e.button.y;
+void Game::handleTouch(float x, float y, bool pressed) {
+    if (pressed) {
+        // Джойстик
+        if (abs(x - controls.joyX) < 45 && abs(y - controls.joyY) < 45) {
+            controls.joyActive = true;
+            controls.joyStickX = x;
+            controls.joyStickY = y;
+        }
+        // Атака
+        if (abs(x - controls.atkX) < 52 && abs(y - controls.atkY) < 52) {
+            controls.atkHold = true;
+        }
+        // Back
         if (x > 20 && x < 160 && y > 20 && y < 75) {
             backToLobby = true;
         }
+    } else {
+        if (controls.atkHold) {
+            controls.atkHold = false;
+            // Создаём пулю
+            Bullet b;
+            b.x = playerX; b.y = playerY;
+            b.vx = aimX * BULLET_SPEED;
+            b.vy = aimY * BULLET_SPEED;
+            bullets.push_back(b);
+        }
+        controls.joyActive = false;
+        controls.joyStickX = controls.joyX;
+        controls.joyStickY = controls.joyY;
     }
 }
 
