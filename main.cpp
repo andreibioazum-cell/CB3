d6cf531a2e255038122f0b2c60688a1bd3932f70
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-21 10:28:39 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+#include "Lobby.hpp"
+#include "Game.hpp"
+
+enum GameState { STATE_LOBBY, STATE_GAME };
+GameState currentState = STATE_LOBBY;
+
+Lobby lobby;
+Game game;
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Cubic Battle 3");
+    window.setFramerateLimit(60);
+    
+    lobby.init(window);
+    game.init(window);
+    
+    sf::Clock clock;
+    while (window.isOpen()) {
+        float dt = clock.restart().asSeconds();
+        if (dt > 0.05f) dt = 0.05f;
+        
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) window.close();
+            if (currentState == STATE_LOBBY) lobby.handleEvent(event);
+            else game.handleEvent(event);
+        }
+        
+        if (currentState == STATE_LOBBY) {
+            if (lobby.update(dt)) currentState = STATE_GAME;
+        } else {
+            game.update(dt);
+            if (game.isBackToLobby()) currentState = STATE_LOBBY;
+        }
+        
+        window.clear();
+        if (currentState == STATE_LOBBY) lobby.draw(window);
+        else game.draw(window);
+        window.display();
+    }
+    return 0;
+}
