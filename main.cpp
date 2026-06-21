33732081feec132d42361ec66fb48124b41a7f0c
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-21 11:52:40 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,56 +1,85 @@
- #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
 #include <jni.h>
 #include <android/log.h>
-#include "lobby.hpp"
-#include "game.hpp"
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include <string>
+#include <vector>
+#include <cmath>
 
 #define LOG_TAG "CubicBattle"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-enum GameState { STATE_LOBBY, STATE_GAME };
-GameState currentState = STATE_LOBBY;
+// ============================================================
+// ПРОСТАЯ ИГРА БЕЗ SFML (OpenGL ES)
+// ============================================================
 
-lobby::Lobby lobby;
-game::Game game;
+struct Point {
+    float x, y;
+};
+
+class Lobby {
+public:
+    bool startGame = false;
+    
+    void update() {}
+    void draw() {}
+};
+
+class Enemy {
+public:
+    Point pos = {400, 300};
+    float angle = 0;
+    int hp = 5;
+    bool alive = false;
+    
+    void reset() { alive = false; }
+};
+
+class Game {
+public:
+    Point player = {640, 360};
+    float angle = 0;
+    int hp = 5;
+    Enemy enemy;
+    bool backToLobby = false;
+    
+    void init() {
+        enemy.reset();
+    }
+    
+    void update(float dt) {
+        // Простая логика
+    }
+    
+    void draw() {
+        // OpenGL рисование
+    }
+};
+
+Lobby lobby;
+Game game;
 
 // ============================================================
-// JNI ФУНКЦИЯ ДЛЯ ЗАПУСКА ИГРЫ ИЗ JAVA
+// JNI ФУНКЦИИ
 // ============================================================
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_cubicbattle_GameActivity_nativeRun(JNIEnv* env, jobject obj, jobject surface) {
-    
-    // Создаём окно для Android
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Cubic Battle 3");
-    window.setFramerateLimit(60);
-    
-    lobby.init(window);
-    game.init(window);
-    
-    sf::Clock clock;
-    
-    while (window.isOpen()) {
-        float dt = clock.restart().asSeconds();
-        if (dt > 0.05f) dt = 0.05f;
-        
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) window.close();
-            if (currentState == STATE_LOBBY) lobby.handleEvent(event);
-            else game.handleEvent(event);
-        }
-        
-        if (currentState == STATE_LOBBY) {
-            if (lobby.update(dt)) currentState = STATE_GAME;
-        } else {
-            game.update(dt);
-            if (game.isBackToLobby()) currentState = STATE_LOBBY;
-        }
-        
-        window.clear();
-        if (currentState == STATE_LOBBY) lobby.draw(window);
-        else game.draw(window);
-        window.display();
-    }
+Java_com_cubicbattle_GameActivity_nativeInit(JNIEnv* env, jobject obj) {
+    LOGI("Game initialized!");
+    game.init();
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_cubicbattle_GameActivity_nativeUpdate(JNIEnv* env, jobject obj, jfloat dt) {
+    game.update(dt);
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_cubicbattle_GameActivity_nativeGetHP(JNIEnv* env, jobject obj) {
+    return game.hp;
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_cubicbattle_GameActivity_nativeGetTitle(JNIEnv* env, jobject obj) {
+    return env->NewStringUTF("CUBIC BATTLE 3");
 }
