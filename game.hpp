2cf0c877a9954bbb8eb1779210d3305edd9467d3
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-22 15:06:16 +0400

    Update game.hpp

--- game.hpp
+++ game.hpp
@@ -13,11 +13,7 @@ struct Bullet {
 
 class Game {
 private:
-    // OpenGL ресурсы
     GLuint program;
-    GLuint vbo;
-    GLuint playerVBO;
-    GLuint enemyVBO;
     
     float playerX = 640, playerY = 360;
     float playerAngle = 0;
@@ -46,9 +42,7 @@ private:
     const float BULLET_SPEED = 340 * 1.15f;
     
     void createShaderProgram();
-    void createMeshes();
     void drawRect(float x, float y, float w, float h, float r, float g, float b);
-    void drawCircle(float x, float y, float radius, float r, float g, float b);
     void drawHPBar(float x, float y, float w, float h, int hp, int max);
     
 public:
